d3a143f5743e710917858050a63be36568185f78
Hanuhitha Dondapati <42184594+Hanuhitha@users.noreply.github.com>
2024-08-11 23:35:38 -0400

    minor changes

--- app.py
+++ app.py
@@ -6,52 +6,52 @@ def read_file(file_path):
     with open(file_path, "r") as file:
         return file.read()
 
+if __name__ == "__main__":
+    graph = execute_graph()
+    st.title("Langchain Smart Q/A system")
+    code_language = st.selectbox("code language", ["python", "Java", "CPP", "None"])
+    query = st.text_area("Enter your question:")
+    if code_language != "None":
+        query = f"""{query}. Write code in {code_language} programming language. """
+    else:
+        query =   f"""Write a summary for {query}"""
 
-graph = execute_graph()
-st.title("Langchain Smart Q/A system")
-code_language = st.selectbox("code language", ["python", "Java", "CPP", "None"])
-query = st.text_area("Enter your question:")
-if code_language != "None":
-    query = f"""{query}. Write code in {code_language} programming language. """
-else:
-     query =   f"""Write a summary for {query}"""
-
-current_state = None
-if st.button("Submit"):
-    if query:
-        with st.status("Thinking.."):
+    current_state = None
+    if st.button("Submit"):
+        if query:
+            with st.status("Thinking.."):
+                try:
+                    for s in graph.stream({"query": query}):
+                        current_state = list(s.keys())[0]
+                        if "__end__" not in s:
+                            st.write(f'Entering : {current_state} node')
+                        else:
+                            st.write(f'Entering : {current_state} node')
+                            break
+                except:
+                    st.error("error occured! Cannot process request. Retry with different prompt.")
             try:
-                for s in graph.stream({"query": query}):
-                    current_state = list(s.keys())[0]
-                    if "__end__" not in s:
-                        st.write(f'Entering : {current_state} node')
-                    else:
-                        st.write(f'Entering : {current_state} node')
-                        break
+                current_state_dict = s[current_state]
+                code_file_path = s[current_state]["code_file_path"]
+                summary_file_path = s[current_state]["summary_file_path"]
+                audio_file_path = s[current_state]["audio_file_path"]
+                
+                if code_file_path:
+                
+                    code_file = read_file(code_file_path)
+                    st.header("Generated Code")
+                    st.code(code_file)
+                if summary_file_path:
+                    summary = read_file(summary_file_path)
+                    st.header("Generated Summary")
+                    summary = [x + "\n" if not (i+1)%15 else x for i,x in enumerate(summary.split(" "))]
+                    st.text(" ".join(summary))
+                    
+                if audio_file_path:
+                    st.header("Audio Summary")
+                    st.audio(audio_file_path)
+                    
+                    
             except:
-                st.error("error occured! Cannot process request. Retry with different prompt.")
-        try:
-            current_state_dict = s[current_state]
-            code_file_path = s[current_state]["code_file_path"]
-            summary_file_path = s[current_state]["summary_file_path"]
-            audio_file_path = s[current_state]["audio_file_path"]
-            
-            if code_file_path:
-            
-                code_file = read_file(code_file_path)
-                st.header("Generated Code")
-                st.code(code_file)
-            if summary_file_path:
-                summary = read_file(summary_file_path)
-                st.header("Generated Summary")
-                summary = [x + "\n" if not (i+1)%15 else x for i,x in enumerate(summary.split(" "))]
-                st.text(" ".join(summary))
-                
-            if audio_file_path:
-                st.header("Audio Summary")
-                st.audio(audio_file_path)
-                
-                
-        except:
-            st.error("LLM unable to process the request")
-            st.error(s)
+                st.error("LLM unable to process the request")
+                st.error(s)
---
+++ code/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int index = 1; index <= p.length(); index++) {
+            if (p.charAt(index - 1) == '*') {
+                dp[0][index] = dp[0][index - 2];
+            }
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
